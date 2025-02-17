6a6a0a0acdcefb42117ca44c4b51a2b9be71be38
Virgiliu <virgiliu.plesca@infigo.net>
2025-02-17 10:29:03 +0200

    added menu for patch runner

--- PatchRunner.bat
+++ PatchRunner.bat
@@ -1,11 +1,51 @@
+@echo off
 cd /d "D:\Projects\catfish\Catfish\Catfish.PatchRunner"
 
-if exist bin\ (
+:menu
+echo ==========================================
+echo             Catfish Menu
+echo ==========================================
+echo [1] CatfishManagement
+echo [2] Update
+echo ==========================================
+set /p choice=Enter your choice: 
 
-    if not exist .\bin\Release\Settings.txt (
-        copy D:\Settings.txt D:\Projects\catfish\Catfish\Catfish.PatchRunner\bin\Release
-        powershell write-host -fore Cyan Copied Settings.txt file.
+if "%choice%"=="1" goto management
+if "%choice%"=="2" goto update
+
+echo Invalid choice. Please try again.
+pause
+goto menu
+
+:management
+if exist .\bin\Release\Settings.txt (
+	del .\bin\Release\Settings.txt
     )
+
+    
+        copy D:\SettingsTxt\CatfishManagement\Settings.txt D:\Projects\catfish\Catfish\Catfish.PatchRunner\bin\Release
+        powershell write-host -fore Cyan Copied Settings.txt file.
+    
+
+    cd /d ".\bin\Release"
+    cls
+    start /B /wait Catfish.PatchRunner.exe task catfishmanagement
+    pause
+) else (
+    cls
+    powershell write-host -fore Red Error: bin/Release folder does not exist. Try to build your project first.
+    pause
+)
+
+
+:update
+if exist .\bin\Release\Settings.txt (
+	del .\bin\Release\Settings.txt
+    )
+
+        copy D:\SettingsTxt\Update\Settings.txt D:\Projects\catfish\Catfish\Catfish.PatchRunner\bin\Release
+        powershell write-host -fore Cyan Copied Settings.txt file.
+    
 
     cd /d ".\bin\Release"
     cls
@@ -13,6 +53,6 @@ if exist bin\ (
     pause
 ) else (
     cls
-    powershell write-host -fore Red Error: bin/Release folder does not exist. Try to build your project firstly
+    powershell write-host -fore Red Error: bin/Release folder does not exist. Try to build your project first.
     pause
 )
